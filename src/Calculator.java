839c2b604b2395108bc84b1fc3aca2a85399bc0a
Jonil Kodra <jonil.kodra@student.tugraz.at>
2021-10-08 19:58:56 +0200

    Variablen Initialisierung

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,6 +2,17 @@ public class Calculator {
 
     public static void main(String[] args) {
 
+        int a =1;
+        System.out.println(a);
+        double b = 2;
+        System.out.println(b);
+        boolean c = true;
+        System.out.println(c);
+        char d = 'J';
+        System.out.println(d);
+        String j = "Jonil";
+        System.out.println(j);
+
     }
 
 }
